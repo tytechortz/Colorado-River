e18eba08e75bbc5818f806baac3b25baf91830ac
James Swank <jmswank7@gmail.com>
2021-09-19 07:30:51 -0600

    cleans up drought

--- apps/drought.py
+++ apps/drought.py
@@ -1,23 +1,9 @@
-import requests
-import csv
-import pandas as pd
 import dash
 from dash import html, dcc
 import plotly.graph_objs as go
-import pandas as pd
-import numpy as np
 from dash.dependencies import Input, Output, State
-import time
-from datetime import datetime, date, timedelta
 from homepage import get_header, get_navbar, get_emptyrow
-# from data import powell_df
 
-today = time.strftime("%Y-%m-%d")
-
-# url = 'https://usdmdataservices.unl.edu/api/StateStatistics/GetDroughtSeverityStatisticsByAreaPercent?aoi=08&startdate=1/1/2000&enddate=' + today + '&statisticsType=2'
-
-
-# print(df)
 
 app = dash.Dash(__name__)
 app.config['suppress_callback_exceptions']=True
@@ -49,42 +35,12 @@ def drought_App():
             ),
             dcc.Interval(
                 id='interval-component',
-                interval=200*1000, # in milliseconds
+                interval=2000*1000, # in milliseconds
                 n_intervals=0
             ),
             dcc.Store(id='drought-data'),   
         ])
     ])
 
-# app.layout = html.Div([
-#     html.Div([
-#         html.H2(
-#             'Drought',
-#             className='twelve columns',
-#             style={'text-align': 'center'}
-#         )
-#     ],
-#         className='row'
-#     ),
-#     html.Div([
-#         dcc.Graph(
-#             id='drought-graph'
-#         )
-#     ],  
-#         className='row'
-#     ),
-#     dcc.Interval(
-#         id='interval-component',
-#         interval=200*1000, # in milliseconds
-#         n_intervals=0
-#     ),
-#     dcc.Store(id='drought-data'),
-# ])
-
-
-
-
-# if __name__ == '__main__':
-#     app.run_server(port=8010, debug=True)
 
 app.layout = drought_App
