850930fd73c214f13014ebd0dc0d68bb8cb3ba77
James Swank <jmswank7@gmail.com>
2021-09-18 21:47:18 -0600

    fixees data download

--- drought.py
+++ drought.py
@@ -3,7 +3,7 @@ import csv
 import pandas as pd
 import io
 
-url = "https://usdmdataservices.unl.edu/api/StateStatistics/GetDroughtSeverityStatisticsByAreaPercent?aoi=08&startdate=1/1/2000&enddate=8/17/2021&statisticsType=1"
+url = "https://usdmdataservices.unl.edu/api/StateStatistics/GetDroughtSeverityStatisticsByAreaPercent?aoi=08&startdate=1/1/2000&enddate=9/17/2021&statisticsType=2"
 
 r = requests.get(url).content
 
@@ -14,4 +14,6 @@ df['date'] = pd.to_datetime(df['MapDate'].astype(str), format='%Y%m%d')
 
 df.drop(['StatisticFormatID', 'StateAbbreviation', 'MapDate'] , axis=1, inplace=True)
 df.set_index('date', inplace=True)
+print(df)
+df['DSCI'] = (df['D0'] + (df['D1']*2) + (df['D2']*3) + (df['D3']*4 + (df['D4']*5)))
 print(df)
